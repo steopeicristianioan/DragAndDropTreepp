0ddbf382e0bda0dd07fcd7e45fdda91dc2a3bbd0
Steopei Cristian Ioan <steopeicristianioan@gmail.com>
2021-09-24 16:31:55 +0300

    90% complete

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace dragAndDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picB2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.picB1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picB2
+            // 
+            this.picB2.Location = new System.Drawing.Point(57, 71);
+            this.picB2.Name = "picB2";
+            this.picB2.Size = new System.Drawing.Size(265, 167);
+            this.picB2.TabIndex = 0;
+            this.picB2.TabStop = false;
+            this.picB2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picB2_MouseDown);
+            // 
+            // panel2
+            // 
+            this.panel2.AllowDrop = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.picB2);
+            this.panel2.Location = new System.Drawing.Point(430, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(359, 308);
+            this.panel2.TabIndex = 1;
+            this.panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel2_DragDrop);
+            this.panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel2_DragEnter);
+            // 
+            // picB1
+            // 
+            this.picB1.Image = ((System.Drawing.Image)(resources.GetObject("picB1.Image")));
+            this.picB1.Location = new System.Drawing.Point(45, 71);
+            this.picB1.Name = "picB1";
+            this.picB1.Size = new System.Drawing.Size(261, 167);
+            this.picB1.TabIndex = 0;
+            this.picB1.TabStop = false;
+            this.picB1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picB1_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.picB1);
+            this.panel1.Location = new System.Drawing.Point(22, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(358, 308);
+            this.panel1.TabIndex = 2;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "s";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picB2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox picB1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dragAndDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Panel main;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            main = new Panel();
+            main.Parent = this;
+            main.Size = new Size(550, 250);
+            main.BackColor = Color.Red;
+            panel1.Parent = main;
+            panel2.Parent = main;
+            panel1.Location = new Point(10, 5);
+            
+        }
+        private void main_dragEnter(object sender, DragEventArgs e)
+        {
+            main.BackColor = Color.Bisque;
+        }
+
+        private void picB1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (picB1.Image != null)
+                {
+                    picB1.DoDragDrop(picB1.Image, DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void panel2_DragEnter(object sender, DragEventArgs e)
+        {
+
+            if (e.Data.GetDataPresent(DataFormats.Bitmap))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+
+        private void panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            picB2.Image= (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            picB1.Image = null;
+        }
+
+        private void picB2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button== MouseButtons.Left && picB2.Image != null)
+            {
+                picB2.DoDragDrop(picB2.Image, DragDropEffects.Move);
+            }
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap))
+                e.Effect = DragDropEffects.Move;
+            else e.Effect = DragDropEffects.None;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            picB1.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            picB2.Image = null;
+        }
+    }
+}
